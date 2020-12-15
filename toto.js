588103c7a3353a8b2c998319a7bf7e6fd586e5ac
paulclement-cmd <paulclement.lr@gmail.com>
2020-12-15 02:25:56 +0000

    ‘commit’

---
+++ toto.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+class App extends React.Component {
+
+
+
+    render() {
+        return <div>
+            <button>
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F661958845210132864%2F&psig=AOvVaw3JmJsKkeeZoVAUmn4LfYRh&ust=1608085362438000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjelfn2zu0CFQAAAAAdAAAAABAJ" alt="Killua"></img>
+            </button>
+
+        </div>
+    }
+
+}
+
+export default App
